616bcb03957f03a80f000780d517034f3766b37d
malteserteresa <tess_the_titan@hotmail.com>
2019-10-18 12:37:26 +0200

    improved preprocessing pipeline notebook

--- src/data/retrieve_data_from_s3_bucket.py
+++ src/data/retrieve_data_from_s3_bucket.py
@@ -1,17 +1,10 @@
-import boto3
 from botocore.exceptions import ClientError
 
 
-def download_dataset(s3_client):
+def download_dataset(s3_client, file_path: str):
     """Downloads the stanford dataset."""
     try:
-        s3_client.download_file(
-            "opt-out-tools-data", "stanford.csv", "../../data/raw/stanford.csv"
-        )
+        s3_client.download_file("opt-out-tools-data", "stanford.csv", file_path)
 
     except ClientError:
         print("Dataset does not exist")
-
-
-if __name__ == "__main__":
-    download_dataset(boto3.client("s3"))
