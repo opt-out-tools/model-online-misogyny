ab91c32b999a89d0b84c67b49b1cad4f51df3dea
malteserteresa <tess_the_titan@hotmail.com>
2019-10-18 14:44:20 +0200

    streamlined download dataset signature and improved notebook text

--- src/data/retrieve_data_from_s3_bucket.py
+++ src/data/retrieve_data_from_s3_bucket.py
@@ -1,10 +1,13 @@
+import boto3
 from botocore.exceptions import ClientError
 
 
-def download_dataset(s3_client, file_path: str):
+def download_dataset(file_path: str):
     """Downloads the stanford dataset."""
+    client = boto3.client("s3")
+
     try:
-        s3_client.download_file("opt-out-tools-data", "stanford.csv", file_path)
+        client.download_file("opt-out-tools-data", "stanford.csv", file_path)
 
     except ClientError:
         print("Dataset does not exist")
