83e4b56981959b27d4beb29a568e817ff8cfbf3a
Matteo Guzzo <guzzo.matteo@gmail.com>
2020-02-20 17:34:50 +0100

    Remove class balancing from prepare stage

--- src/prepare.py
+++ src/prepare.py
@@ -3,21 +3,17 @@ import pandas as pd
 
 
 def main():
+    """Here one wold implement preliminary operations e.g. removing NAs"""
     args = sys.argv[1:]
-    input_file = args[1]
-    output_file = args[2]
+    input_file = args[0]
+    output_file = args[1]
     print(f"Input: {input_file}")
     print(f"Output: {output_file}")
     df_in = pd.read_csv(input_file)
     print("Input DF info:")
     df_in.info()
 
-    # Balance classes
-    # Take the number of elements in the minority class
-    min_comm_elements = min(df_in.groupby("label").count().to_numpy())[0]
-    df_positive = df_in[df_in["label"] == 1].sample(min_comm_elements, random_state=42)
-    df_negative = df_in[df_in["label"] == 0].sample(min_comm_elements, random_state=42)
-    df_balanced = pd.concat((df_negative, df_positive)).reset_index(drop=True)
+    df_balanced = df_in
     print("Output DF info:")
     df_balanced.info()
 
