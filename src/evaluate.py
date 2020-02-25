a4a1d67e3e52d2433a128a2c45a0ab41548e4dc1
Matteo Guzzo <guzzo.matteo@gmail.com>
2020-02-25 16:03:07 +0100

    Properly remove Dvcfile

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,3 @@
-import os
 import sys
 from time import time
 from pathlib import Path
@@ -44,7 +43,6 @@ def load_artifacts(test_set_file, trained_model_file):
 def write_results(output_folder, y_test, y_prob):
     """Save to disk results of model evaluation"""
     roc_img_file_png = output_folder / "roc_auc_f1.png"
-    roc_img_file_pdf = output_folder / "roc_auc_f1.pdf"
     metrics_file = output_folder / "eval.json"
 
     with plt.style.context("ggplot"):
@@ -52,10 +50,6 @@ def write_results(output_folder, y_test, y_prob):
 
     with open(roc_img_file_png, "wb") as handler:
         plt.savefig(handler, dpi=150, format="png")
-    with open(roc_img_file_pdf, "wb") as handler:
-        # Creation date must be off for reproducibility
-        os.environ["SOURCE_DATE_EPOCH"] = ""
-        plt.savefig(handler, dpi=150, format="pdf", metadata={"creationDate": None})
 
     metrics = dict(f1=f1_test, AUC=auc_test)
     with open(metrics_file, "w") as file:
